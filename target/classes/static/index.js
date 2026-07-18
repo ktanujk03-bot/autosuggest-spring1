50e6e75bcc0e4f996f1cd2cc6330a89398bf0cd7
Katikathala Tanuj <ktanujk03@gmail.com>
2026-07-18 12:55:39 +0530

    toggle and random

---
+++ target/classes/static/index.js
@@ -0,0 +1,57 @@
+var users = [
+    {
+        "name" : "The Strongest of Today",
+        "gender" : "Sorcerer",
+        "img" : "gojo.png"
+    },
+    {
+        "name" : "The Strongest In History",
+        "gender" : "Cursed Spirit",
+        "img" : "sukuna.png"
+    },
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id +1)%2
+
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+
+}
+
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+            
+            return response.json();
+        })
+        
+        .then(function(data){
+
+            var userData = data.results[0];
+
+            
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " +
+                                    userData.name.last;
+
+            
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        
+        .catch(function(err){
+            console.log(err);
+        })
+}
